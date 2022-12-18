ae0dbfd2df699c0d79861c9accc4a18e324882d8
Raigardas <raigardas.tautkus@knf.stud.vu.lt>
2022-12-18 15:39:48 +0200

    initial commit

---
+++ constants/index.js
@@ -0,0 +1,92 @@
+export const exploreWorlds = [
+  {
+    id: 'world-1',
+    imgUrl: '/planet-01.png',
+    title: 'The Hogwarts',
+  },
+  {
+    id: 'world-2',
+    imgUrl: '/planet-02.png',
+    title: 'The Upside Down',
+  },
+  {
+    id: 'world-3',
+    imgUrl: '/planet-03.png',
+    title: 'Kadirojo Permai',
+  },
+  {
+    id: 'world-4',
+    imgUrl: '/planet-04.png',
+    title: 'Paradise Island',
+  },
+  {
+    id: 'world-5',
+    imgUrl: '/planet-05.png',
+    title: 'Hawkins Labs',
+  },
+]
+
+export const startingFeatures = [
+  'Find a world that suits you and you want to enter',
+  'Enter the world by reading basmalah to be safe',
+  'No need to beat around the bush, just stay on the gas and have fun',
+]
+
+export const newFeatures = [
+  {
+    imgUrl: '/vrpano.svg',
+    title: 'A new world',
+    subtitle:
+      'A new world for you to explore, with a new way to interact with the world',
+  },
+  {
+    imgUrl: '/headset.svg',
+    title: 'More realistic',
+    subtitle:
+      'In the latest update, your eyes are narrow, making the world more realistic than ever',
+  },
+]
+
+export const insights = [
+  {
+    imgUrl: '/planet-06.png',
+    title: 'The launch of the Metaverse is just the beginning',
+    subtitle:
+      'The world has just launched the Metaverse, and it is just the beginning of the Metaverse. The Metaverse is a new world that is more',
+  },
+  {
+    imgUrl: '/planet-07.png',
+    title: 'Cybermod is the future of the Metaverse',
+    subtitle:
+      'Cybermod created by Raigardas Tautkus has transformed the Metaverse into an easily accessible and user-friendly world. ',
+  },
+  {
+    imgUrl: '/planet-08.png',
+    title: 'With one platform you can explore the whole world virtually',
+    subtitle:
+      'Cybermod allows the user to easily explore the whole world virtually with the help of a VR headset. ',
+  },
+]
+
+export const socials = [
+  {
+    href: 'https://www.linkedin.com/in/raigardas-tautkus/',
+    name: 'linkedin',
+    url: '/linkedin.svg',
+  },
+  {
+    href: 'https://www.facebook.com/eRaigardasTautkus/',
+    name: 'facebook',
+    url: '/facebook.svg',
+  },
+  {
+    href: 'https://twitter.com',
+    name: 'twitter',
+    url: '/twitter.svg',
+  },
+  {
+    href: 'https://www.instagram.com/',
+    name: 'instagram',
+    url: '/instagram.svg',
+  },
+]
---
+++ sections/Feedback.jsx
@@ -0,0 +1,65 @@
+'use client'
+
+import { motion } from 'framer-motion'
+
+import styles from '../styles'
+import { fadeIn, staggerContainer, zoomIn } from '../utils/motion'
+
+const Feedback = () => (
+  <section className={`${styles.paddings}`}>
+    <motion.div
+      variants={staggerContainer}
+      initial='hidden'
+      whileInView='show'
+      viewport={{ once: false, amount: 0.25 }}
+      className={`${styles.innerWidth} mx-auto flex lg:flex-row flex-col gap-6`}
+    >
+      <motion.div
+        variants={fadeIn('right', 'tween', 0.2, 1)}
+        className='flex-[0.5] lg:max-w-[370px] flex justify-end flex-col gradient-05 sm:p-8 p-4 rounded-[32px] border-[1px] border-[#6A6A6A] relative'
+      >
+        <div className='feedback-gradient' />
+        <div>
+          <h4 className='font-bold sm:text-[32px] text-[26px] sm:leading-[40.32px] leading-[36.32px] text-white'>
+            Raigardas
+          </h4>
+          <p className='mt-[8px] font-normal sm:text-[18px] text-[12px] sm:leading-[22.68px] leading-[16.68px] text-white'>
+            Founder of Cybermod
+          </p>
+        </div>
+
+        <p className='mt-[24px] font-normal sm:text-[24px] text-[18px] sm:leading-[45.6px] leading-[39.6px] text-white'>
+          “With the development of today's technology, Cybermod is very useful
+          in practice. It's what people call Web 3.0. By joining the Cybermod,
+          you take part of the new world.”
+        </p>
+      </motion.div>
+
+      <motion.div
+        variants={fadeIn('left', 'tween', 0.2, 1)}
+        className='relative flex-1 flex justify-center items-center'
+      >
+        <img
+          src='/planet-09.png'
+          alt='planet-09'
+          className='w-full lg:h-[610px] h-auto min-h-[210px] object-cover rounded-[40px]'
+        />
+
+        <motion.div
+          variants={zoomIn(0.4, 1)}
+          className='lg:block hidden absolute -left-[10%] top-[3%]'
+        >
+          <a href='#explore'>
+            <img
+              src='/stamp.png'
+              alt='stamp'
+              className='w-[155px] h-[155px] object-contain rotate-180'
+            />
+          </a>
+        </motion.div>
+      </motion.div>
+    </motion.div>
+  </section>
+)
+
+export default Feedback
